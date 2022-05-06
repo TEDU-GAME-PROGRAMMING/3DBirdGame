551e3f06c2f5e189dc354c3e8c593acae35d4bba
ArxEge <egegap@hotmail.com>
2022-05-06 10:31:22 +0300

    3D vol3

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit !!! ");
+        Application.Quit();
+    }
+    public void Levels()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}

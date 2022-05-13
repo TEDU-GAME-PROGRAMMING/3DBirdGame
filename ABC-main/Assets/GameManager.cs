b7577b5ba5bad492dd4a8eb275062c4457edb86e
bariscankayabasii <bcan.kayabasi@tedu.edu.tr>
2022-05-13 21:27:05 +0300

    Vol11
    AudioFixed

---
+++ ABC-main/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+
+    public GameObject completeLevel;
+    public void CompleteLevel()
+    {
+        completeLevel.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", 1f);
+
+
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ ABC-main/Assets/LevelComplete.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+
+    public void LoadNextLevel()
+    {
+        int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        PlayerPrefs.SetInt("reachedLevel", nextLevel);
+        SceneManager.LoadScene("LevelSelect");
+    }
+}
